0df3344c89460f6c40e9440fc4b14139ddc0c3a5
zearkiatos <caprilespe@outlook.com>
2023-06-17 21:24:14 -0400

    Fetch data

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Description
 This is a javascript 🟨 project kata 🥋 to learn, practice asynchronism in javascript
 
+![Github](https://github.com/zearkiatos/javascript-asynchronism--kata/actions/workflows/action.yml/badge.svg)
+
 # Made with
 [![JavaScript](https://img.shields.io/badge/javascript-ead547?style=for-the-badge&logo=javascript&logoColor=white&labelColor=000000)]()
--- tests/callback/challenge.acceptation.test.js
+++ tests/callback/challenge.acceptation.test.js
@@ -9,12 +9,36 @@ describe("Challenge suite acceptation test", () => {
   test("Should get an error", () => {
     const consoleLogSpyOn = jest.spyOn(console, "log");
     challenge.fetchData(config.PLATZI_FAKE_API_BASE_URL, (error, data) => {
-      if(error) {
+      if (error) {
         console.log(error);
         return error;
       }
       return data;
     });
+
+    expect(consoleLogSpyOn).toHaveBeenCalled();
+  });
+
+  test("Should get data", () => {
+    const consoleLogSpyOn = jest.spyOn(console, "log");
+    challenge.fetchData(
+      `${config.PLATZI_FAKE_API_BASE_URL}/products`,
+      (error, data) => {
+        if (error) return console.error(error);
+        fetchData(
+          `${config.PLATZI_FAKE_API_BASE_URL}/products/${data[0].id}`,
+          function (error2, data2) {
+            if (error2) return console.error(error2);
+            fetchData(`${config.PLATZI_FAKE_API_BASE_URL}/categories/${data2?.category?.id}`, function(error3, data3) {
+              if(error3) return console.error(error);
+              console.log(data[0]);
+              console.log(data2.title);
+              console.log(data3.name);
+            })
+          }
+        );
+      }
+    );
 
     expect(consoleLogSpyOn).toHaveBeenCalled();
   });
