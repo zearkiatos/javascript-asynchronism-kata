e848d9b0674f4a7c6dc2b8e9c902834f693db632
zearkiatos <caprilespe@outlook.com>
2023-06-19 23:40:53 -0400

    What are the promises

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    name: "javascript-asynchronism-kata",
-    verbose: false,
-    testMatch: ["**/*.test.js"],
-    testEnvironment: "node",
-    collectCoverageFrom: ["src/**/*.js", "!**/node_modules/**"],
-    coverageThreshold: {
-      global: {
-        statements:61,
-        branches: 40,
-        lines: 67,
-        functions: 72,
-      },
+  name: "javascript-asynchronism-kata",
+  verbose: false,
+  testMatch: ["**/*.test.js"],
+  testEnvironment: "node",
+  collectCoverageFrom: ["src/**/*.js", "!**/node_modules/**"],
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      branches: 53,
+      lines: 75,
+      functions: 80,
     },
-  };
+  },
+};
